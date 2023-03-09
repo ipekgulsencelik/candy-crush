bc8e90c8f754c0333b0c26d35e6e6a9e3b6f84ba
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-09 21:07:17 +0300

    importing images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 
+// image
+import blueCandy from './images/blue-candy.png'
+import greenCandy from './images/green-candy.png'
+import orangeCandy from './images/orange-candy.png'
+import purpleCandy from './images/purple-candy.png'
+import redCandy from './images/red-candy.png'
+import yellowCandy from './images/yellow-candy.png'
+import blank from './images/blank.png'
+
 const width = 8;
 const candyColors = [
   blueCandy,
