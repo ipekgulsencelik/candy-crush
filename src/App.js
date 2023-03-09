bc43cbfcb60db16f01d63911ca271aaa3e2b4c3d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-09 21:35:25 +0300

    Checking for matches

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 // image
-import blueCandy from './images/blue-candy.png'
-import greenCandy from './images/green-candy.png'
-import orangeCandy from './images/orange-candy.png'
-import purpleCandy from './images/purple-candy.png'
-import redCandy from './images/red-candy.png'
-import yellowCandy from './images/yellow-candy.png'
-import blank from './images/blank.png'
+import blueCandy from "./images/blue-candy.png";
+import greenCandy from "./images/green-candy.png";
+import orangeCandy from "./images/orange-candy.png";
+import purpleCandy from "./images/purple-candy.png";
+import redCandy from "./images/red-candy.png";
+import yellowCandy from "./images/yellow-candy.png";
+import blank from "./images/blank.png";
 
 const width = 8;
 const candyColors = [
@@ -22,6 +22,97 @@ const candyColors = [
 function App() {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i <= 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (
+        columnOfFour.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
+        )
+      ) {
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  };
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55, 62, 63, 64,
+      ];
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfFour.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
+        )
+      ) {
+        rowOfFour.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  };
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i <= 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (
+        columnOfThree.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  };
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
+        )
+      ) {
+        rowOfThree.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -35,6 +126,23 @@ function App() {
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour();
+      checkForRowOfFour();
+      checkForColumnOfThree();
+      checkForRowOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [
+    checkForColumnOfFour,
+    checkForRowOfFour,
+    checkForColumnOfThree,
+    checkForRowOfThree,
+    currentColorArrangement,
+  ]);
 
   return (
     <div className="app">
